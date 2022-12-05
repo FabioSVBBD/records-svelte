1f26a62a1ba80c3a0d1c55fc6ad861114edf9da4
Fabio Sousa Vieira <fabio.sousavieira@vcontractor.co.za>
2022-12-05 15:08:36 +0200

    scaffold

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:svelte/recommended'],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'svelte/rule-name': 'error'
+    'svelte/button-has-type': 'error',
+  },
+};
